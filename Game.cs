b21d97e4e26387b23e462f9ee9ab68cf1acf1353
kugelrund <marcel.jacobse@ewetel.net>
2016-06-25 23:03:37 +0200

    Added documentation for Game.cs and Settings.cs

--- Game.cs
+++ Game.cs
@@ -3,67 +3,179 @@ using System.Diagnostics;
 
 namespace LiveSplit.ComponentAutosplitter
 {
+    /// <summary>
+    /// Defines an abstract Game. Stores basic information about a game
+    /// that are essential to know for the autosplitter, most importantly
+    /// what types of events to split on are available for a game.
+    /// </summary>
     abstract class Game
     {
+        /// <summary>
+        /// Array of the possible Type's that split-events can
+        /// have for this game.
+        /// </summary>
         public abstract Type[] EventTypes { get; }
 
+        /// <summary>
+        /// Full name of the game.
+        /// </summary>
         public abstract string Name { get; }
+        /// <summary>
+        /// Executable name of the game.
+        /// </summary>
         public abstract string ProcessName { get; }
 
+        /// <summary>
+        /// Allows to define a function that reads a GameEvent from an xml string
+        /// directly as a fallback for old autosplitter settings that don't use
+        /// the save system used by this version of the ComponentAutosplitter.
+        /// </summary>
+        /// <param name="id">
+        /// The "id" of the event that has to be interpreted by this function
+        /// to output the corresponding GameEvent;
+        /// </param>
+        /// <returns></returns>
         public virtual GameEvent ReadLegacyEvent(string id)
         {
+            // in general we don't know so just return an EmptyEvent
             return new EmptyEvent();
         }
     }
 
+    /// <summary>
+    /// Provides properties and functions to gather information about a game.
+    /// This is a partial class because it is expected to be extended for each
+    /// game that shall use this ComponentAutosplitter (an inheritance approach
+    /// caused a lot of issues with the GameEvent class and didn't work well
+    /// at all but please tell me if you have a better idea).
+    /// </summary>
     partial class GameInfo
     {
+        /// <summary>
+        /// The process that information is gathered for.
+        /// </summary>
         private Process gameProcess;
+        /// <summary>
+        /// Base address of the process.
+        /// </summary>
         private IntPtr baseAddress;
 
+        /// <summary>
+        /// Provides read access to "gameProcess".
+        /// </summary>
         public Process GameProcess => gameProcess;
+        /// <summary>
+        /// Whether the game is currently ingame, e.g. not in a loading state.
+        /// </summary>
         public bool InGame { get; protected set; }
 
+        /// <summary>
+        /// Creates a GameInfo object for the give process.
+        /// </summary>
+        /// <param name="gameProcess">
+        /// The process that information shall be gathered for.
+        /// </param>
         public GameInfo(Process gameProcess)
         {
             this.gameProcess = gameProcess;
             baseAddress = gameProcess.MainModule.BaseAddress;
         }
 
+        /// <summary>
+        /// Wrapper that calls the UpdateInfo function to update information
+        /// about the game.
+        /// </summary>
         public void Update()
         {
             UpdateInfo();
         }
 
+        /// <summary>
+        /// Updates information about the game. This is expected to
+        /// be implemented for each specific game
+        /// </summary>
         partial void UpdateInfo();
     }
 
+    /// <summary>
+    /// Defines an abstract GameEvent, i.e. an event that can happen in
+    /// the game that can issue the autosplitter to split. Specific game
+    /// implementations will define events that inherit from this class.
+    ///
+    /// When you create a Event class make sure to provide a parameterless
+    /// constructor that will be needed in the ChangeEventForm (i wish
+    /// i could force you to somehow but i didn't find a way to do that).
+    ///
+    /// Overriding ToString also makes sense cause it will be used in the
+    /// Settings form.
+    /// </summary>
     abstract class GameEvent
     {
+        /// <summary>
+        /// Names of optional attributes that a GameEvent can have.
+        /// </summary>
         public abstract string[] AttributeNames { get; }
+        /// <summary>
+        /// Values of optional attributes that a GameEvent can have.
+        /// </summary>
         public abstract string[] AttributeValues { get; protected set; }
+        /// <summary>
+        /// Description for this GameEvent that can be shown in dialogs.
+        /// </summary>
         public abstract string Description { get; }
 
+        /// <summary>
+        /// Most important function for a GameEvent. This function has
+        /// to decide whether the event occured, based on the info provided
+        /// by a GameInfo objet.
+        /// </summary>
+        /// <param name="info">
+        /// The GameInfo object that provides info about the game state.
+        /// </param>
+        /// <returns>
+        /// <code>true</code> when the event happenen, <code>false</code>
+        /// when it did not.
+        /// </returns>
         public abstract bool HasOccured(GameInfo info);
     }
 
+    /// <summary>
+    /// Abstract GameEvent that depends on a map of the game.
+    /// </summary>
     abstract class MapEvent : GameEvent
     {
+        /// <summary>
+        /// A MapEvent depends on a map, so it has a matching attribute
+        /// </summary>
         private static readonly string[] attributeNames = { "Map" };
 
         public override string[] AttributeNames => attributeNames;
         public override string[] AttributeValues { get; protected set; }
 
+        /// <summary>
+        /// The specific map that this MapEvent depends on.
+        /// </summary>
         protected readonly string map;
 
+        /// <summary>
+        /// Parameterless constructor for ChangeEventForm.
+        /// </summary>
         public MapEvent()
         {
             map = "";
             AttributeValues = new string[] { "" };
         }
 
+        /// <summary>
+        /// Creates a map event based on the given map.
+        /// </summary>
+        /// <param name="map">
+        /// The name of the map to base this event on.
+        /// </param>
         public MapEvent(string map)
         {
+            // TODO: meh this should not be here cause it's engine specific
+            //       but i guess it works for a lot of games...
             if (map.EndsWith(".bsp"))
             {
                 this.map = map;
@@ -77,10 +189,13 @@ namespace LiveSplit.ComponentAutosplitter
         }
     }
 
+    /// <summary>
+    /// Provides an EmptyEvent, i.e. an event that never happens.
+    /// </summary>
     class EmptyEvent : GameEvent
     {
-        private static readonly string[] attributeNames = new string[0] { };
-        private static string[] attributeValues = new string[0] { };
+        private static readonly string[] attributeNames = new string[] { };
+        private static string[] attributeValues = new string[] { };
 
         public override string[] AttributeNames => attributeNames;
         public override string[] AttributeValues
@@ -92,6 +207,7 @@ namespace LiveSplit.ComponentAutosplitter
 
         public override bool HasOccured(GameInfo info)
         {
+            // empty event, never happens
             return false;
         }
 
--- Settings.cs
+++ Settings.cs
@@ -6,10 +6,22 @@ using LiveSplit.Model;
 
 namespace LiveSplit.ComponentAutosplitter
 {
+    /// <summary>
+    /// Settings control for a ComponentAutosplitter. Deals with
+    /// saving, loading and letting the user change settings, most
+    /// importantly a list of events that define when to split.
+    /// </summary>
     partial class Settings : UserControl
     {
+        /// <summary>
+        /// The game that the autosplitter shall be used for.
+        /// </summary>
         private Game game;
 
+        /// <summary>
+        /// Stuff for signaling when the eventlist changes so that the
+        /// Component class can properly use the new eventlist.
+        /// </summary>
         private bool eventsChanged = false;
         public event EventHandler EventsChanged;
         protected virtual void OnChanged(EventArgs e)
@@ -17,9 +29,24 @@ namespace LiveSplit.ComponentAutosplitter
             EventsChanged?.Invoke(this, e);
         }
 
+        /// <summary>
+        /// Setting of whether to use GameTime.
+        /// </summary>
         public bool PauseGameTime { get; private set; }
+        /// <summary>
+        /// Current list of segments of the run. Used in the DataGridView for the
+        /// user to conveniently set events to split on for specific segments.
+        /// </summary>
         public IRun Segments { private get; set; }
 
+
+
+        /// <summary>
+        /// Constructs the settings object for the ComponentAutosplitter.
+        /// </summary>
+        /// <param name="game">
+        /// The game that the autosplitter shall be used for.
+        /// </param>
         public Settings(Game game)
         {
             InitializeComponent();
@@ -27,6 +54,14 @@ namespace LiveSplit.ComponentAutosplitter
             PauseGameTime = true;
         }
 
+        /// <summary>
+        /// Collect the events to split on from the DataGridView into an array. The
+        /// Component class uses this method to get the eventlist, so that it knows
+        /// when to split.
+        /// </summary>
+        /// <returns>
+        /// The array of the current list of events.
+        /// </returns>
         public GameEvent[] GetEventList()
         {
             int length = dgvSegmentEvents.Rows.Count;
@@ -43,8 +78,10 @@ namespace LiveSplit.ComponentAutosplitter
         }
 
         /// <summary>
-        /// Updates the list of segments in the DataGridView. Also adds or removes rows
-        /// of the DataGridView to match the amount of segments.
+        /// Updates the list of segments in the DataGridView based on the contents
+        /// of the "Segments" member. The Component class updates that member when
+        /// the segments changed. Also adds or removes rows of the DataGridView to
+        /// match the amount of segments.
         /// </summary>
         public void UpdateSegments()
         {
@@ -70,28 +107,106 @@ namespace LiveSplit.ComponentAutosplitter
             dgvSegmentEvents.Rows[Segments.Count].Cells[Segment.Index].Value = "--- End ---";
         }
 
-        private void ClearEvents()
-        {
-            foreach (DataGridViewRow row in dgvSegmentEvents.Rows)
-            {
-                row.Cells[Event.Index].Value = "";
-            }
-            eventsChanged = true;
-        }
-
+        /// <summary>
+        /// Opens a dialog for the user to change the event in the topmost
+        /// selected row.
+        /// </summary>
         private void ChangeEvent()
         {
+            // get selected cell
             DataGridViewCell eventCell = dgvSegmentEvents.SelectedRows[0].Cells[Event.Index];
+
+            // open form
             ChangeEventForm form = new ChangeEventForm(game, eventCell.Value as GameEvent);
             form.ShowDialog();
+
+            // set new event
             eventCell.Value = form.NewEvent;
             eventsChanged = true;
         }
 
+        /// <summary>
+        /// Deletes all events in the DataGridView.
+        /// </summary>
+        private void ClearEvents()
+        {
+            foreach (DataGridViewRow row in dgvSegmentEvents.Rows)
+            {
+                row.Cells[Event.Index].Value = null;
+            }
+            eventsChanged = true;
+        }
+
+        /// <summary>
+        /// Changes the order of the currently selected events in the DataGridView
+        /// by moving them up or down.
+        /// </summary>
+        /// <param name="up">
+        /// Whether to move upwards or downwards.
+        /// </param>
+        private void MoveEvents(bool up)
+        {
+            // To properly deal with moving multiple selected events we have
+            // to get the indices of the selected rows in the correct order.
+            // We cannot use "SelectedRows" directly for that cause the rows in
+            // there are not sorted.
+            int[] selectedIndices = new int[dgvSegmentEvents.SelectedRows.Count];
+            for (int i = 0; i < dgvSegmentEvents.SelectedRows.Count; i += 1)
+            {
+                selectedIndices[i] = dgvSegmentEvents.SelectedRows[i].Index;
+            }
+            // sort the indices
+            Array.Sort(selectedIndices);
+            if (!up)
+            {
+                // When we want to move down we need to move the bottom
+                // rows first so reverse the order.
+                Array.Reverse(selectedIndices);
+            }
+
+            // Now that we have the sorted indices of the selected rows
+            // we can move the rows.
+            int newIndex;
+            object tempValue;  // temporary storage for swapping
+            foreach (int oldIndex in selectedIndices)
+            {
+                // new index for this row
+                newIndex = up ? oldIndex - 1 : oldIndex + 1;
+                // Don't move the row if it would get moved out of the list
+                if (newIndex >= 0 && newIndex < dgvSegmentEvents.Rows.Count)
+                {
+                    // swap row[oldIndex] and row[newIndex]
+                    tempValue = dgvSegmentEvents.Rows[oldIndex].Cells[Event.Index].Value;
+                    dgvSegmentEvents.Rows[oldIndex].Cells[Event.Index].Value =
+                        dgvSegmentEvents.Rows[newIndex].Cells[Event.Index].Value;
+                    dgvSegmentEvents.Rows[newIndex].Cells[Event.Index].Value = tempValue;
+                    // change selection so that the moved row stays selected
+                    dgvSegmentEvents.Rows[newIndex].Selected = true;
+                    dgvSegmentEvents.Rows[oldIndex].Selected = false;
+                }
+            }
+
+            // order changed so we have to rebuild list of events
+            eventsChanged = true;
+        }
+
+        /// <summary>
+        /// Save the current settings to xml.
+        /// </summary>
+        /// <param name="document">
+        /// The XmlDocument to store the settings in.
+        /// </param>
+        /// <returns>
+        /// The created settings node.
+        /// </returns>
         public XmlNode GetSettings(XmlDocument document)
         {
+            // create settingsNode
             XmlElement settingsNode = document.CreateElement("settings");
 
+            // create node for storing the current list of events. For each
+            // event it's type (derived from GameEvent) will be saved together
+            // with it's AttributeValues.
             XmlElement usedEventsNode = document.CreateElement("usedEvents");
             XmlElement eventNode;
             XmlElement typeNode;
@@ -99,16 +214,20 @@ namespace LiveSplit.ComponentAutosplitter
             GameEvent gameEvent;
             foreach (DataGridViewRow row in dgvSegmentEvents.Rows)
             {
+                // get event from row
                 gameEvent = row.Cells[Event.Index].Value as GameEvent;
                 if (gameEvent == null)
                 {
                     gameEvent = new EmptyEvent();
                 }
+
+                // save derived type of the event
                 eventNode = document.CreateElement("event");
                 typeNode = document.CreateElement("type");
                 typeNode.InnerText = gameEvent.GetType().ToString();
                 eventNode.AppendChild(typeNode);
 
+                // save all attribute values
                 foreach (string attributeValue in gameEvent.AttributeValues)
                 {
                     attributeNode = document.CreateElement("attribute");
@@ -120,6 +239,7 @@ namespace LiveSplit.ComponentAutosplitter
             }
             settingsNode.AppendChild(usedEventsNode);
 
+            // save setting of whether to use game time
             XmlElement pauseGameTimeNode = document.CreateElement("pauseGameTime");
             pauseGameTimeNode.InnerText = PauseGameTime.ToString();
             settingsNode.AppendChild(pauseGameTimeNode);
@@ -127,23 +247,37 @@ namespace LiveSplit.ComponentAutosplitter
             return settingsNode;
         }
 
+        /// <summary>
+        /// Loads settings from xml.
+        /// </summary>
+        /// <param name="settings">
+        /// The xml node to read from.
+        /// </param>
         public void SetSettings(XmlNode settings)
         {
             if (settings["usedEvents"] != null)
             {
+                // make enough rows in the DataGridView for the events in the xml
                 if (settings["usedEvents"].ChildNodes.Count > dgvSegmentEvents.Rows.Count)
                 {
-                    dgvSegmentEvents.Rows.Add(settings["usedEvents"].ChildNodes.Count - dgvSegmentEvents.Rows.Count);
+                    dgvSegmentEvents.Rows.Add(settings["usedEvents"].ChildNodes.Count -
+                                              dgvSegmentEvents.Rows.Count);
                 }
 
+                // Read all events and store them in the DataGridView
                 int i = 0;
+                GameEvent gameEvent;
+                Type type;
+                List<string> attributes;
                 foreach (XmlNode eventNode in settings["usedEvents"].ChildNodes)
                 {
-                    GameEvent gameEvent;
                     if (eventNode.FirstChild.HasChildNodes)
                     {
-                        Type type = Type.GetType(eventNode.FirstChild.InnerText);
-                        List<string> attributes = new List<string>();
+                        // get the type of the event.
+                        type = Type.GetType(eventNode.FirstChild.InnerText);
+
+                        // read the attributes of the event
+                        attributes = new List<string>();
                         foreach (XmlNode node in eventNode.ChildNodes)
                         {
                             if (node != eventNode.FirstChild)
@@ -152,6 +286,7 @@ namespace LiveSplit.ComponentAutosplitter
                             }
                         }
 
+                        // Create a new GameEvent object based on the read type and attributes
                         gameEvent = Activator.CreateInstance(type, attributes.ToArray()) as GameEvent;
                     }
                     else
@@ -160,10 +295,12 @@ namespace LiveSplit.ComponentAutosplitter
                         gameEvent = game.ReadLegacyEvent(eventNode.InnerText);
                     }
 
+                    // store the event in the DataGridView
                     dgvSegmentEvents.Rows[i].Cells[Event.Index].Value = gameEvent;
                     i += 1;
                 }
 
+                // signal that events have changed
                 OnChanged(EventArgs.Empty);
                 eventsChanged = false;
             }
@@ -171,13 +308,9 @@ namespace LiveSplit.ComponentAutosplitter
             bool pauseGameTime;
             if (settings["pauseGameTime"] != null && Boolean.TryParse(settings["pauseGameTime"].InnerText, out pauseGameTime))
             {
+                // read setting of whether to use game time
                 PauseGameTime = pauseGameTime;
             }
-        }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            ClearEvents();
         }
 
         private void btnChangeEvent_Click(object sender, EventArgs e)
@@ -185,53 +318,9 @@ namespace LiveSplit.ComponentAutosplitter
             ChangeEvent();
         }
 
-        private void Settings_HandleDestroyed(object sender, EventArgs e)
+        private void btnClear_Click(object sender, EventArgs e)
         {
-            if (eventsChanged)
-            {
-                eventsChanged = false;
-                OnChanged(EventArgs.Empty);
-            }
-        }
-
-        private void dgvSegmentEvents_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.ColumnIndex == Event.Index)
-            {
-                ChangeEvent();
-            }
-        }
-
-        private void MoveEvents(bool up)
-        {
-            int[] selectedIndices = new int[dgvSegmentEvents.SelectedRows.Count];
-            for (int i = 0; i < dgvSegmentEvents.SelectedRows.Count; i += 1)
-            {
-                selectedIndices[i] = dgvSegmentEvents.SelectedRows[i].Index;
-            }
-            Array.Sort(selectedIndices);
-            if (!up)
-            {
-                Array.Reverse(selectedIndices);
-            }
-
-            int newIndex;
-            object tempValue;
-            foreach (int oldIndex in selectedIndices)
-            {
-                newIndex = up ? oldIndex - 1 : oldIndex + 1;
-                if (newIndex >= 0 && newIndex < dgvSegmentEvents.Rows.Count)
-                {
-                    tempValue = dgvSegmentEvents.Rows[oldIndex].Cells[Event.Index].Value;
-                    dgvSegmentEvents.Rows[oldIndex].Cells[Event.Index].Value =
-                        dgvSegmentEvents.Rows[newIndex].Cells[Event.Index].Value;
-                    dgvSegmentEvents.Rows[newIndex].Cells[Event.Index].Value = tempValue;
-                    dgvSegmentEvents.Rows[newIndex].Selected = true;
-                    dgvSegmentEvents.Rows[oldIndex].Selected = false;
-                }
-            }
-
-            eventsChanged = true;
+            ClearEvents();
         }
 
         private void btnMoveDown_Click(object sender, EventArgs e)
@@ -242,6 +331,23 @@ namespace LiveSplit.ComponentAutosplitter
         private void btnMoveUp_Click(object sender, EventArgs e)
         {
             MoveEvents(true);
+        }
+
+        private void dgvSegmentEvents_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == Event.Index)
+            {
+                ChangeEvent();
+            }
+        }
+
+        private void Settings_HandleDestroyed(object sender, EventArgs e)
+        {
+            if (eventsChanged)
+            {
+                eventsChanged = false;
+                OnChanged(EventArgs.Empty);
+            }
         }
     }
 }
