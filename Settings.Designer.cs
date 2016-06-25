c6f377920d27635495c659f7cb73f5b7ad8063ed
kugelrund <marcel.jacobse@ewetel.net>
2016-06-25 12:46:04 +0200

    Replaced german VS comments with english

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -2,15 +2,15 @@
 {
     partial class Settings
     {
-        /// <summary> 
-        /// Erforderliche Designervariable.
+        /// <summary>
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
-        /// Verwendete Ressourcen bereinigen.
+        /// <summary>
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,11 +20,11 @@
             base.Dispose(disposing);
         }
 
-        #region Vom Komponenten-Designer generierter Code
+        #region Windows Form Designer generated code
 
-        /// <summary> 
-        /// Erforderliche Methode für die Designerunterstützung. 
-        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -157,6 +157,7 @@
         }
 
         #endregion
+
         private System.Windows.Forms.Label lblUsedEvents;
         private System.Windows.Forms.CheckBox chkPauseGameTime;
         private System.Windows.Forms.Button btnChangeEvent;
